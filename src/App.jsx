fbab678358729d8da55c6c233d064a562f1737c4
Astrrokid <kidstarro22@gmail.com>
2023-02-27 23:01:08 +0100

    second

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css';
+import Sidebar from './components/Sidebar.jsx'
+import Main from './components/Main.jsx'
+import { useState } from 'react';
+import uuid from 'react-uuid';
+
+
+function App() {
+  const [notes, setNotes] = useState([])
+  const [activeNote, setActiveNote] = useState(false)
+
+  const onAddNote= ()=>{
+    const newNote={
+      id: uuid(),
+      title: "Untitled Note",
+      body: "",
+      lastModified: Date.now()
+    };
+
+    setNotes([newNote, ...notes]); 
+  }
+
+  const onDeleteNote = (idToDelete) =>{
+    setNotes(notes.filter((note)=> note.id !== idToDelete))
+  }
+
+   const getActiveNote= ()=>{
+     return notes.find((note) => note.id === activeNote)
+  }
+  return (
+    <div className="App">
+      <Sidebar notes={notes} 
+      onAddNote={onAddNote} 
+      onDeleteNote={onDeleteNote}
+      activeNote= {activeNote}
+      setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+function Main({activeNote}){
+    return <div className="app-main">
+        <div className="app-main-note-edit">
+            <input type="text" name="" id="title" autoFocus value={activeNote.title} onChange={(e)=> onEditField("title", e.target.value )} />
+            <textarea name="" id="body" cols="30" rows="10" placeholder="Write here..." value={activeNote.body}
+            onChange={(e)=> onEditField("body", e.target.value )}></textarea>
+        </div>
+        <div className="app-main-note-preview">
+            <h1 className="preview-title">
+            {activeNote.title}
+            </h1>
+            <div className="markdown-preview">{activeNote.body}</div>
+        </div>
+    </div>;
+}
+
+export default Main;
